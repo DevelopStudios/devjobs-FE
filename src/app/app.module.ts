8139a56499f349c3f57e3e23f86eadce030652de
Charl Roux <charlroux641@gmail.com>
2024-12-31 12:12:05 +0200

    list view and search bar scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListViewComponent } from './components/list-view/list-view.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListViewComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
