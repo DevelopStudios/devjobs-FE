5549c06e72031330517d9ce56595c0b5803836c2
Charl Roux <charlroux641@gmail.com>
2025-04-13 14:13:27 +0200

    Improved search and show more function

--- src/app/components/list-view/list-view.component.ts
+++ src/app/components/list-view/list-view.component.ts
@@ -1,7 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 import { ListService } from 'src/app/list.service';
+
+interface Listing {
+  id: number,
+  company: string,
+  logo: string,
+  logoBackground: string,
+  position: string,
+  postedAt: string,
+  contract: string,
+  location: string,
+  website: string,
+  apply: string,
+  description: string,
+  requirements: {
+    content: string,
+    items: string[]
+    },
+  role: {
+  content: string,
+  items: string[]
+  }
+}
+
 
 @Component({
   selector: 'app-list-view',
@@ -9,11 +31,13 @@ import { ListService } from 'src/app/list.service';
   styleUrls: ['./list-view.component.scss']
 })
 export class ListViewComponent implements OnInit {
-  listData:any[]=[];
-  listDataShort:any[]=[];
-  data:any[]=[];
-  showMore = false;
-  
+  Listings:Listing[]=[];
+  filteredListings:Listing[]=[];
+  diplayedListings: Listing[]=[];
+  itemsToShowInitially = 5;
+  itemsToLoadOnScroll = 5;
+  showMoreButtonVisible = false;
+  searchParams: any = {};
   constructor(
     private List: ListService,    private router: Router
   ) { }
@@ -21,20 +45,18 @@ export class ListViewComponent implements OnInit {
   ngOnInit(): void {
      this.getListData();
      this.List.searchObject.subscribe(value => {
-        if(Object.keys(value).length === 0){
-          this.listDataShort = this.data;
-        } else {
-          this.searchList(value);
-        }
+      if (Object.keys(value).length > 0){
+        this.searchList(value);
+      }
      });
   }
 
   getListData() {
-    const data = this.List.getListData();
-    this.listData = Object.keys(data).map((key:any) => ({index:key, value: data[key]})).slice(0, -2);
-    this.data = Object.keys(data).map((key:any) => ({index:key, value: data[key]})).slice(0, -8);
-    const mappedShort = Object.keys(data).map((key:any) => ({index:key, value: data[key]})).slice(0, -8);
-    this.listDataShort = mappedShort;
+    this.applyFilterAndPaginate();
+    let data:Listing[] = this.List.getListData();
+    this.Listings = Object.entries(data).slice(0, -2).map(([key,value]) => value as Listing);
+    this.diplayedListings = this.Listings.slice(0, this.itemsToShowInitially);
+    this.showMoreButtonVisible = this.Listings.length > this.diplayedListings.length;
   }
 
   getImageUrl(item:any) {
@@ -43,22 +65,26 @@ export class ListViewComponent implements OnInit {
 
   searchList(param:any) {
     const paramCopy = JSON.parse(JSON.stringify(param));
-   
     if(paramCopy !== false){
       if(paramCopy.fulltime === true) {
         paramCopy.fulltime = 'Full Time';
       } else {
         paramCopy.fulltime = '';
       }
-      let data = [...this.listDataShort].filter(obj => 
-        obj.value.position.toLowerCase().includes(paramCopy.general.toLowerCase())
-        &&
-        obj.value.location.toLowerCase().includes(paramCopy.location.toLowerCase())
-        &&
-        obj.value.contract.toLowerCase().includes(paramCopy.fulltime.toLowerCase())
-      );
-      this.listDataShort = data;
+      this.searchParams = paramCopy;
+      this.applyFilterAndPaginate();
     }
+  }
+
+  applyFilterAndPaginate(){
+    this.filteredListings = this.Listings.filter(obj => {
+      const generalMatch = obj.position.toLowerCase().includes(this.searchParams.general.toLowerCase());
+      const locationMatch = obj.location.toLowerCase().includes(this.searchParams.location.toLowerCase());
+      const fulltimeMatch = obj.contract.toLowerCase().includes(this.searchParams.fulltime.toLowerCase());
+      return generalMatch && locationMatch && fulltimeMatch;
+    });
+    this.diplayedListings = this.filteredListings.slice(0, this.itemsToShowInitially);
+    this.showMoreButtonVisible = this.filteredListings.length > this.diplayedListings.length;
   }
 
   toggleEntry(param:any) {
@@ -67,13 +93,11 @@ export class ListViewComponent implements OnInit {
   }
 
   toggleShowMore() {
-    if(this.showMore === false){
-      this.listDataShort = this.listData;
-      this.showMore = true;
-    } else {
-      this.listDataShort = this.data;
-      this.showMore = false;
-    }
-    
+    const nextItems = this.Listings.slice(
+      this.diplayedListings.length,
+      this.diplayedListings.length + this.itemsToLoadOnScroll
+    );
+    this.diplayedListings = [...this.diplayedListings,...nextItems];
+    this.showMoreButtonVisible = this.Listings.length > this.diplayedListings.length;
   }
 }
