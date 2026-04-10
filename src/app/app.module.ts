51e2b4541a938bbdcca4f14a233609f8bc90d17e
Charl <charlroux641@gmail.com>
2026-04-10 14:49:26 -0400

    version 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListViewComponent } from './components/list-view/list-view.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { EditViewComponent } from './components/edit-view/edit-view.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-@NgModule({
-  declarations: [
-    AppComponent,
-    ListViewComponent,
-    SearchBarComponent,
-    EditViewComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        ListViewComponent,
+        SearchBarComponent,
+        EditViewComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
